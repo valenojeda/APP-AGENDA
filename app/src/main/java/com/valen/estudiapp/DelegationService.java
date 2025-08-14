0902216761436997acec0ec9272a9d62a36d55e7
Valen Ojeda <valenojeda94@gmail.com>
2025-08-13 22:44:27 -0300

    Iconos

---
+++ app/src/main/java/com/valen/estudiapp/DelegationService.java
@@ -0,0 +1,14 @@
+package com.valen.estudiapp;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
